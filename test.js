818b4b49f8ce7252de2eda079497e1903f710a8a
XMarcXYV <marcxyver.gica@wlcormoc.edu.ph>
2025-11-30 14:27:20 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,19 @@
+// test.js
+
+// Simple functions to test Git commits
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+// Test the functions
+console.log(greet("Git User"));
+console.log("Add 5 + 3 =", add(5, 3));
+console.log("Subtract 5 - 3 =", subtract(5, 3));
